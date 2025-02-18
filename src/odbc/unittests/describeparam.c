30423d45bf81059d4709a0456d677bba68833b03
Frediano Ziglio <freddy77@gmail.com>
2025-02-18 20:39:38 +0000

    save, comments

--- src/odbc/unittests/describeparam.c
+++ src/odbc/unittests/describeparam.c
@@ -273,6 +273,17 @@ TEST_MAIN()
 	check_int(digits, ==, 0);
 	check_int(nullable, ==, SQL_NULLABLE);
 
+	// TODO try to understand which output get into sql type and others
+	// suggested_tds_type_id for type
+	// also suggested_precision and sugested_scale (like NUMERIC but also for INT)
+	// for varchar length from suggested_tds_length
+	// "name" could be useful to check which argument
+
+
+	// TODO check if query cannot return types (see examples from MS
+	// sp_describe_undeclared_parameters)
+	// For instance SELECT 1 WHERE ? = ?
+
 	/*****************************************************************/
 
 	/* cleanup */
