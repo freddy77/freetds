bb1b6e763a0310d91888682d10956300d7dca968
Matt McNabb <matthew.mcnabb@vmssoftware.com>
2025-11-18 14:11:19 +1300

    Issue #686: bcp in: Read length-prefixed data correctly
    Signed-off-by: Matt McNabb <matthew.mcnabb@vmssoftware.com>

--- src/dblib/bcp.c
+++ src/dblib/bcp.c
@@ -1250,8 +1250,13 @@ _bcp_read_hostfile(DBPROCESS *dbproc, TDSFILESTREAM *stream, bool *row_error, bo
 			}
 		}
 
-		/* if (Max) column length specified take that into consideration. (Meaning what, exactly?) */
+		/* Or if data file does not use a length prefix for this column,
+		 * work out the length to read based on the size of a fixed data type.
+		 */
+		else if (is_fixed_type(hostcol->datatype))
+			collen = tds_get_size_by_type(hostcol->datatype);
 
+		/* if (Max) column length specified take that into consideration. (Meaning what, exactly?) */
 		if (!data_is_null && hostcol->column_len >= 0) {
 			if (hostcol->column_len == 0)
 				data_is_null = true;
@@ -1262,11 +1267,6 @@ _bcp_read_hostfile(DBPROCESS *dbproc, TDSFILESTREAM *stream, bool *row_error, bo
 		}
 
 		tdsdump_log(TDS_DBG_FUNC, "prefix_len = %d collen = %d \n", hostcol->prefix_len, collen);
-
-		/* Fixed Length data - this overrides anything else specified */
-
-		if (is_fixed_type(hostcol->datatype))
-			collen = tds_get_size_by_type(hostcol->datatype);
 
 		col_start = tds_file_stream_tell(stream);
 
